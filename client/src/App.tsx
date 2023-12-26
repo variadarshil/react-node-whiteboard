13af87e13f94305973f04396045977ccddc97611
Darshil Varia <darshilvaria@Darshils-MacBook-Air.local>
2023-12-26 22:55:22 +0530

    whiteboard react-node

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './components/Board'
+
+function App() {
+
+  const [brushColor, setBrushColor] = useState('black');
+  const [brushSize, setBrushSize] = useState<number>(5);
+  return (
+    <div className='App'>
+      <h1>Collaborative Whiteboard</h1>
+      <div>
+        <Board brushColor={brushColor} brushSize={brushSize} />
+        <div className='tools'>
+          <div>
+            <span>Color: </span>
+            <input type='color' value={brushColor} onChange={(e) => setBrushColor(e.target.value)} />
+          </div>
+          <div>
+            <span>Size: </span>
+            <input type='range' color='#fac176'
+              min="1" max="100" value={brushSize} onChange={(e) => setBrushSize(Number(e.target.value))} />
+            <span>{brushSize}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
